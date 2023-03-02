432623a8313213f421286bf23b0e42f1e31ba15c
Hegeldirkk <hegeldirkk@gmail.com>
2023-03-02 14:15:58 +0000

    island perimeyter

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -8,4 +8,25 @@ INTERVIEW: Lockboxes
 
 def canUnlockAll(boxes):
     """check if boxes can opened"""
-
+    n = len(boxes)
+    keys = boxes[0]
+    locked = []
+    print(boxes[0][0])
+    for i in range(n):
+        if i > 0:
+            locked.append(i)
+    print(locked)
+    for x in range(n):
+        if len(boxes[x]) == 1:
+            if boxes[x][0] in locked:
+                result = locked.index(boxes[x][0])
+                locked.pop(result)
+                locked.insert(result, None)
+        else:
+            for y in range(len(boxes[x])):
+                if boxes[x][y] in locked:
+                    result = locked.index(boxes[x][y])
+                    locked.pop(result)
+                    locked.insert(result, None)
+            print(locked)
+    return locked == []
