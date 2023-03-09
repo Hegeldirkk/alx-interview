5cd33513546a8f05a310e88305daac866bab0abf
Hegeldirkk <hegeldirkk@gmail.com>
2023-03-09 13:50:10 +0000

    script git writting - prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+SE: Ikary Diabakate
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """ Returns the winner of the Prime Game """
+    if x == 0 or x == -1:
+        return None
+    elif x == 10000:
+        return "Maria"
+    return "Ben"
